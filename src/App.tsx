aaa850318f3858670c6f40f6a7282c3a0bc92964
codingle2 <kpss0337@gmail.com>
2025-03-13 21:12:33 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "@pages/index";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<MainPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function index() {
+  return <div>index 페이지 입니다.</div>;
+}
+
+export default index;
